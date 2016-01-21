3d1b3a5f43aa27b1bd89a7f26064701e1a394d56
Martin Ben <mebenav88@gmail.com>
2016-01-21 14:03:45 -0500

    add hello world class

--- lib/hello_medev21.rb
+++ lib/hello_medev21.rb
@@ -1,5 +1,11 @@
 require "hello_medev21/version"
 
-module HelloMedev21
-  # Your code goes here...
+# module HelloMedev21
+#   # Your code goes here...
+# end
+
+class HelloMedev21
+  def self.hi
+    puts "Hello world!"
+  end
 end
